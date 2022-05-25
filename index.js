458830827b7571f1d47878e59e4f22944bc1fe58
unknown <ahmednahid1995@gmail.com>
2022-05-26 01:28:01 +0600

    app.use included for middleware

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+require('dotenv').config();
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.send('cyber slice server is running');
