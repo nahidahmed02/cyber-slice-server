85da557ea840952ce9dfc2a896d85bc444c6f422
unknown <ahmednahid1995@gmail.com>
2022-05-26 02:19:19 +0600

    connected to db

--- index.js
+++ index.js
@@ -3,11 +3,30 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fh4wlpf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('db connected');
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
