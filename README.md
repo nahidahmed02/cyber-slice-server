c6a2855f151b3475b14bc250895749c583f259e5
unknown <ahmednahid1995@gmail.com>
2022-05-29 16:49:17 +0600

    verify admin implimented

---
+++ README.md
@@ -0,0 +1,12 @@
+# Cyber Slice
+[Live Site Link](https://github.com/facebook/create-react-app).
+
+
+## Feature and Functionality
+
+* A logged in user can order
+* An admin is able to add/delete parts
+* Admin can make a user an admin
+* Logged in user can give review and update his/her profile
+* Confidential routes are protected
+* Informations are stored in database
--- index.js
+++ index.js
@@ -41,6 +41,16 @@ async function run() {
         const orderCollection = client.db('cyber_slice').collection('order');
         const profileCollection = client.db('cyber_slice').collection('profile');
 
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                next();
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden' });
+            }
+        }
 
         // ------------------ USERS -------------------
 
@@ -67,7 +77,7 @@ async function run() {
 
 
         // to give user a admin role
-        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
             const email = req.params.email;
             const filter = { email: email };
             const updateDoc = {
@@ -75,6 +85,13 @@ async function run() {
             };
             const result = await userCollection.updateOne(filter, updateDoc);
             res.send(result);
+        });
+
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
         });
 
 
