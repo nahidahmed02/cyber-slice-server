935054cd5b0af889b40cbf4788d04d711e9e41be
unknown <ahmednahid1995@gmail.com>
2022-05-29 21:31:59 +0600

    api to delete a parts

--- index.js
+++ index.js
@@ -122,6 +122,14 @@ async function run() {
             res.send(result);
         });
 
+        // to delete a parts
+        app.delete('/parts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await partsCollection.deleteOne(query);
+            res.send(result);
+        });
+
         // ----------------------------------------------
 
 
