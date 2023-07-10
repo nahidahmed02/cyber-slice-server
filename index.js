28f9da9e6d0579db8b4fd29dfc0d57052e900a1d
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-07-10 21:45:25 +0600

    removed expiration time for users

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ async function run() {
                 $set: user,
             };
             const result = await userCollection.updateOne(filter, updateDoc, options);
-            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '23h' });
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET);
             res.send({ result, token });
         });
 
